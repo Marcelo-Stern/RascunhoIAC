6bbd9a9b6bdfa1389a8bec7509c844a0a28e3fd5
Marcelo-Stern <marcelo.silva776@etec.sp.gov.br>
2022-10-24 10:55:09 -0300

    Calculadora Funcionando

--- app/src/main/java/com/example/iaccalc/MainActivity.kt
+++ app/src/main/java/com/example/iaccalc/MainActivity.kt
@@ -12,35 +12,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val quad: EditText = findViewById(R.id.edtQuadril)
+        val alt: EditText = findViewById(R.id.edtAltura)
         val btCalc: Button = findViewById(R.id.btnCalc)
+
+        fun iac(): Double {
+            val inputQuad = quad.text.toString()
+            val inputAlt = alt.text.toString()
+
+            val quad = inputQuad.toDouble()
+            val alt = inputAlt.toDouble()
+
+            val resultIAC = quad / (alt * kotlin.math.sqrt(alt)) - 18
+            return resultIAC
+        }
+
+        fun calcIAC(){
+            if(quad.text.isEmpty() || alt.text.isEmpty()){
+                Toast.makeText(this, "Preencha os campos corretamente para efetuar o cálculo!", Toast.LENGTH_SHORT).show()
+                return
+            }else{
+                val resultado = iac()
+                Toast.makeText(this, "O seu Índice de Adiposidade Corporal é: $resultado", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+
         btCalc.setOnClickListener{
             calcIAC()
         }
-    }
-
-    private val quad: EditText = findViewById(R.id.edtQuadril)
-    private val alt: EditText = findViewById(R.id.edtAltura)
-
-    private fun calcIAC(){
-        if(quad.text.isEmpty() || alt.text.isEmpty()){
-            Toast.makeText(this, "Preencha os campos corretamente para efetuar o cálculo!", Toast.LENGTH_SHORT).show()
-            return
-        }else{
-            val resultado = iac()
-            Toast.makeText(this, "O seu Índice de Adiposidade Corporal é: $resultado", Toast.LENGTH_SHORT).show()
-            return
-        }
-    }
-
-    private fun iac(): Double {
-        val inputQuad = quad.text.toString()
-        val inputAlt = alt.text.toString()
-
-        val quad = inputQuad.toDouble()
-        val alt = inputAlt.toDouble()
-
-        val denom = alt * kotlin.math.sqrt(alt)
-        val resultIAC = quad / denom - 18
-        return resultIAC
     }
 }
